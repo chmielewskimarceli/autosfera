b976553bfd7096ad88147d01e3f4ae950c18f43e
chmielewskimarceli <themanwhosoldtheworld1333@gmail.com>
2026-03-23 03:00:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 document.getElementById('waitlistForm').onsubmit = async (e) => {
   e.preventDefault();
-
   const form = e.target;
-  const container = form.closest('.signup-form');
   const statusMessage = document.getElementById('form-status');
-  const data = new FormData(form);
   const button = form.querySelector('button');
-
-  container.style.height = container.offsetHeight + 'px';
 
   button.innerText = "WYSYŁANIE...";
   button.disabled = true;
@@ -15,26 +10,21 @@ document.getElementById('waitlistForm').onsubmit = async (e) => {
   try {
     const response = await fetch(form.action, {
       method: 'POST',
-      body: data,
+      body: new FormData(form),
       headers: { 'Accept': 'application/json' }
     });
 
     if (response.ok) {
-      form.classList.add('fade-out');
 
-      setTimeout(() => {
-        form.style.display = 'none';
-        
-        statusMessage.style.marginTop = "40px"; 
-        statusMessage.classList.add('fade-in');
-      }, 400);
-
+      form.style.opacity = '0';
+      form.style.pointerEvents = 'none'; 
+      
+      statusMessage.style.display = 'block';
     } else {
       throw new Error();
     }
   } catch (error) {
     button.innerText = "BŁĄD. SPRÓBUJ PONOWNIE";
     button.disabled = false;
-    container.style.height = 'auto'; 
   }
-}
+};
