cf4da539237a0a04a0147e33dc7f2148f2c681ba
chmielewskimarceli <themanwhosoldtheworld1333@gmail.com>
2026-03-23 01:19:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-  document.getElementById('waitlistForm').onsubmit = (e) => {
-    e.preventDefault();
-    alert('Dziękujemy! Poinformujemy Cię o starcie Autosfery.');
-  };
+document.getElementById('waitlistForm').onsubmit = (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const statusMessage = document.getElementById('form-status');
+
+  form.style.display = 'none';
+
+  statusMessage.style.display = 'block';
+}
